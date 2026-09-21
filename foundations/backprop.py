809d5d354f0a26eb4bbe161cf74eacc14e7599c4
Ethan Hans Harianto <79932428+ethanharianto@users.noreply.github.com>
2026-09-21 14:09:14 -0400

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -14,7 +14,7 @@ class Solution:
         # Loss: L = 0.5 * (y_hat - y_true)^2
         # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
         z = np.dot(x, w) + b
-        y_hat = (1/ (1 + np.exp(-z)))
-        dL_dw = np.dot(np.dot((y_hat - y_true),np.dot(y_hat,1-y_hat)), x)
-        dL_db = np.dot((y_hat - y_true),(np.dot(y_hat,1-y_hat)))
-        return (np.round(dL_dw,5), np.round(dL_db,5))
+        y_hat = 1 / (1 + np.exp(-z))
+        dL_dw = np.round(np.dot(np.dot((y_hat - y_true), y_hat * (1-y_hat)), x), 5)
+        dL_db = np.round(np.dot((y_hat - y_true), y_hat * (1-y_hat)), 5)
+        return (dL_dw, dL_db)
